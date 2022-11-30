409a6538f59bd7f904ffdc0d9327913829c79c6e
Hokwan-Cho <HoKwan-Cho@github.com>
2022-11-30 19:02:41 +0900

    TEST 구동방법 각주 추가 .

--- front-end/react-app/src/App.js
+++ front-end/react-app/src/App.js
@@ -1,3 +1,11 @@
+
+
+//1. back-end localhost:8080 구동 (Spring Application)
+//2. fonrt-end server 구동 (react-app 폴더에서 npm run start)
+//3.  localhost:8080/api/crud/insert 해서 데이터 사입
+//4. localhost:3000 으로 데이터 불러와지는지 확인
+
+
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
