34099b7e86faa9821a0e62f3a4a37f27edae1cdb
ansangjae <ansangja@gmail.com>
2022-11-30 19:26:25 +0900

    commit test 2022.11.30

--- back-end/demo/src/main/java/com/example/demo/user/UserRepositorytest.java
+++ back-end/demo/src/main/java/com/example/demo/user/UserRepositorytest.java
@@ -24,7 +24,7 @@ public class UserRepositorytest {
             .email("test01@gmail.com")
             .phone("010-1234-5678")
             .createdBy("admin")
-            .updatedBy("Glenn")
+            .updatedBy("Glenn_안상재")
             .build();
     // Create! 테이블 posts에 id 값이 있으면 update, 없으면 insert 수행
     userRepository.save(user);
